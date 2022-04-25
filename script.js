7a89252a43b1208b583be99a58e7bed60be76d45
evgeniyrabotyaga <evgeniyrabotyaga@gmail.com>
2022-04-25 15:00:00 +0300

    cleaner code

--- script.js
+++ script.js
@@ -13,35 +13,31 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-const darkMode = function () {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  image1.src = img1dark;
-  image2.src = img2dark;
-  image3.src = img3dark;
-};
-
-const lightMode = function () {
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  image1.src = img1light;
-  image2.src = img2light;
-  image3.src = img3light;
+const toggler = function (isDark) {
+  nav.style.backgroundColor = isDark
+    ? 'rgb(0 0 0 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = isDark
+    ? 'rgb(255 255 255 / 50%)'
+    : 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+  isDark
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  image1.src = isDark ? img1dark : img1light;
+  image2.src = isDark ? img2dark : img2light;
+  image3.src = isDark ? img3dark : img3light;
 };
 
 const switchTheme = function (e) {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    darkMode();
+    toggler(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    lightMode();
+    toggler(false);
   }
 };
 
@@ -52,6 +48,6 @@ if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
-    darkMode();
+    toggler(true);
   }
 }
